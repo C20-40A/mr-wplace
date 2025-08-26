46a8aed4ddc2b93a1fa76ee5d68eb243332823a9
c20 <c20.tango@gmail.com>
2025-08-26 12:00:48 +0900

    refactor: storageを分離

---
+++ src/features/favorite/config.ts
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  selectors: {
+    favoriteButton: '[title="お気に入り"]',
+    toggleOpacityButton: 'button[title="Toggle art opacity"]',
+    saveContainer: ".hide-scrollbar.flex.max-w-full.gap-1\\.5.overflow-x-auto",
+  },
+};
