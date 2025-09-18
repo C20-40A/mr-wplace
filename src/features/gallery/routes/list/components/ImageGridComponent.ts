5682db6d033ba334052ee61942b1aeee5a264e4a
Satoshi Tomita <11.1010.100.x2@gmail.com>
2025-09-18 10:33:19 +0900

    chore: ボタンの位置の調節

--- src/features/gallery/routes/list/components/ImageGridComponent.ts
+++ src/features/gallery/routes/list/components/ImageGridComponent.ts
@@ -160,10 +160,10 @@ export class ImageGridComponent {
   private createGotoPositionButtonHtml(item: ImageItem): string {
     return `
       <button 
-        class="btn btn-xs btn-circle btn-ghost absolute bottom-1 left-1 z-10 opacity-70 hover:opacity-100 border border-gray-200 shadow-sm" 
+        class="btn btn-xs btn-circle btn-ghost opacity-70 hover:opacity-100 border border-gray-200 shadow-sm" 
         data-goto-position="${item.key}"
         title="Go to map position"
-        style="background-color: #f3f4f6;"
+        style="position: absolute; bottom: 0.25rem; left: 2rem; z-index: 10; background-color: #f3f4f6;"
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-3" style="color: #059669;">
           <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
@@ -187,10 +187,10 @@ export class ImageGridComponent {
 
     return `
       <button 
-        class="btn btn-xs btn-circle btn-ghost absolute top-1 left-1 z-10 opacity-70 hover:opacity-100 border border-gray-200 shadow-sm" 
+        class="btn btn-xs btn-circle btn-ghost opacity-70 hover:opacity-100 border border-gray-200 shadow-sm" 
         data-draw-toggle="${item.key}"
         title="${isEnabled ? "Hide drawing" : "Show drawing"}"
-        style="${bgStyle}"
+        style="position: absolute; bottom: 0.25rem; left: 0.25rem; z-index: 10; ${bgStyle}"
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-3" style="${eyeStyle}">
           ${eyeIcon}
