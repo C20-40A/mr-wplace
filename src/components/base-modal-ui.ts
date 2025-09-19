bb3aafa77ef0f580cf6a50da16a458202f54a164
c20 <c20.tango@gmail.com>
2025-09-19 21:03:01 +0900

    refactor: modalの統合

---
+++ src/components/base-modal-ui.ts
@@ -0,0 +1,49 @@
+import { Router } from "../utils/router";
+import { createModal, ModalElements } from "../utils/modal";
+
+export interface ModalConfig {
+  id: string;
+  title: string;
+}
+
+/**
+ * Modal UI統一基盤クラス
+ */
+export abstract class BaseModalUI<T extends Router<string>> {
+  protected modalElements: ModalElements;
+  protected router: T;
+
+  constructor(router: T) {
+    this.router = router;
+    this.createModal();
+  }
+
+  abstract getModalConfig(): ModalConfig;
+
+  private createModal(): void {
+    const config = this.getModalConfig();
+    this.modalElements = createModal({
+      id: config.id,
+      title: config.title,
+      hasBackButton: false,
+      onBack: () => this.router.navigateBack(),
+    });
+
+    this.router.setHeaderElements(
+      this.modalElements.titleElement,
+      this.modalElements.backButton
+    );
+  }
+
+  showModal(): void {
+    this.modalElements.modal.showModal();
+  }
+
+  closeModal(): void {
+    this.modalElements.modal.close();
+  }
+
+  getContainer(): HTMLElement | null {
+    return this.modalElements.container;
+  }
+}
