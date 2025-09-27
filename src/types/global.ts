e923f51595ebae2c2bc1ec0ccff6e2d10489415d
c20 <c20.tango@gmail.com>
2025-09-28 06:32:07 +0900

    refactor: type更新

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,20 +1,28 @@
 // mrWplace グローバル型定義
 
+import { ExtendedBookmarks } from "../features/bookmark";
+import { ColorFilter } from "../features/color-filter";
+import { Drawing } from "../features/drawing";
+import { DrawingLoader } from "../features/drawing-loader";
+import { Gallery } from "../features/gallery";
+import { GalleryImageEditor } from "../features/gallery/routes/image-editor";
+import { TileOverlay } from "../features/tile-overlay";
 import type { TimeTravel } from "../features/time-travel";
+import { TileSnapshot } from "../features/time-travel/utils/tile-snapshot";
 import type { UserStatus } from "../features/user-status";
 import type { ColorFilterManager } from "../utils/color-filter-manager";
 
 interface mrWplace {
-  gallery: any;
-  tileOverlay: any;
-  favorites: any;
-  drawing: any;
-  tileSnapshot: any;
+  gallery: Gallery;
+  tileOverlay: TileOverlay;
+  favorites: ExtendedBookmarks;
+  drawing: Drawing;
+  tileSnapshot: TileSnapshot;
   timeTravel: TimeTravel;
-  drawingLoader: any;
-  colorFilter: any;
+  drawingLoader: DrawingLoader;
+  colorFilter: ColorFilter;
   // Optional properties (動的に追加される可能性)
-  imageEditor?: any;
+  imageEditor?: GalleryImageEditor;
   userStatus: UserStatus;
   colorFilterManager?: ColorFilterManager;
 }
