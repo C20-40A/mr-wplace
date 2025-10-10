021ad868a12f53dde41d33f52c9b51c6512e38d5
Satoshi Tomita <11.1010.100.x2@gmail.com>
2025-10-10 12:27:42 +0900

    fix: 「現在のタイルを保存」ボタンがあふれる

--- src/features/time-travel/routes/snapshot-route.ts
+++ src/features/time-travel/routes/snapshot-route.ts
@@ -73,10 +73,6 @@ export class SnapshotRoute extends BaseSnapshotRoute {
     return `
       <div style="margin-top: 8px;">
         <button id="wps-save-current-snapshot-btn" class="btn btn-primary w-full">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
-            <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
-            <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
-          </svg>
           ${t`${"save_current_snapshot"}`}
         </button>
       </div>
@@ -244,7 +240,7 @@ export class SnapshotRoute extends BaseSnapshotRoute {
       "wps-current-tile-canvas"
     ) as HTMLCanvasElement;
     const noImageMessage = document.getElementById("no-image-message");
-    
+
     if (!canvas || !noImageMessage) return;
 
     // 現在位置がない場合
