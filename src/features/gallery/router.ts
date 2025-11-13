0da823c88783c65cb7c054037e1f8a121761d3ae
Satoshi Tomita <11.1010.100.x2@gmail.com>
2025-11-14 04:37:17 +0900

    refactor: update imports

--- src/features/gallery/router.ts
+++ src/features/gallery/router.ts
@@ -1,11 +1,16 @@
-import { Router } from "../../utils/router";
+import { Router } from "@/utils/router";
 
-export type GalleryRoute = "list" | "image-editor" | "image-detail" | "image-share" | "image-selector";
+export type GalleryRoute =
+  | "list"
+  | "image-editor"
+  | "image-detail"
+  | "image-share"
+  | "image-selector";
 
 export class GalleryRouter extends Router<GalleryRoute> {
   constructor() {
     const titleMap: Record<GalleryRoute, string> = {
-      "list": "gallery",
+      list: "gallery",
       "image-editor": "image_editor",
       "image-detail": "image_detail",
       "image-share": "image_share",
