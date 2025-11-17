68ee5eb6c17b53ef5d4a8cbec060d1fd3b7ae937
Satoshi Tomita <11.1010.100.x2@gmail.com>
2025-11-18 04:39:40 +0900

    fix: popupのcontentロード問題

--- src/features/time-travel/routes/base-snapshot-route.ts
+++ src/features/time-travel/routes/base-snapshot-route.ts
@@ -2,7 +2,7 @@ import { TimeTravelStorage, SnapshotInfo } from "../storage";
 import { Toast } from "../../../components/toast";
 import { t, formatDate } from "../../../i18n/manager";
 import { di } from "../../../core/di";
-import { sendSnapshotsToInject } from "@/content";
+import { sendSnapshotsToInject } from "@/utils/inject-bridge";
 
 export abstract class BaseSnapshotRoute {
   protected setupSnapshotEvents(
